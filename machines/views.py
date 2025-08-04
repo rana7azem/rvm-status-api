59a1c53fc6dd598e04cdaf6c47c5213db13213b1
rana7azem <ranahazem816@gmail.com>
2025-08-04 12:42:52 +0300

    Adds the filter function

--- machines/views.py
+++ machines/views.py
@@ -3,11 +3,28 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import RVM
 from .serializers import RVMSerializer
+from django.db.models import Q
 
 class RVMListCreateAPIView(APIView):
 
     def get(self, request):
         rvms = RVM.objects.all()
+        #apply filtering by status
+        status_filter = request.query_params.get('status', None)
+        if status_filter:
+            rvms = rvms.filter(Q(status=status_filter) | Q(status__icontains=status_filter))
+
+        #apply ordering by last_usage
+        order_by = request.query_params.get('order_by', 'last_usage')
+        if order_by:
+            rvms = rvms.order_by(order_by)
+
+        #apply search by location
+        search = request.query_params.get('search', None)
+        if search:
+            rvms = rvms.filter(location__icontains=search)
+        
+        
         serializer = RVMSerializer(rvms, many=True)
         return Response(serializer.data)
 
--- rvmapi/settings.py
+++ rvmapi/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'machines', 
+    'django_filters',
     'rest_framework',
   ]  
 
@@ -123,3 +124,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ]
+}
